fbaf26395e6405041e986f4e92e512785bd8cd77
dvlpr2003 <raja2003dgl@gmail.com>
2026-02-11 14:39:36 +0530

    Refactor Home component to handle pagination and update NotFound link structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,10 @@ export default function NotFound() {
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h2>Not Found 🔍</h2>
       <p>Could not find that Pokemon.</p>
-      <Link href="/" style={{ color: "blue", textDecoration: "underline" }}>
+      <Link
+        href={`/?page=${1}`}
+        style={{ color: "blue", textDecoration: "underline" }}
+      >
         Return Home
       </Link>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,50 @@
 import Card from "@/components/card";
-import FilterBox from "@/components/filter";
-
+import { notFound } from "next/navigation";
 import Link from "next/link";
-interface DataProp {
+interface objectProps {
+  id: number;
   name: string;
   status: string;
   gender: string;
   image: string;
 }
-interface RickmortyProp {
-  results: DataProp[];
+interface dataProps {
+  results: objectProps[];
 }
-export default async function Home() {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
-  const data: RickmortyProp = await res.json();
-  console.log(data.results[0].image);
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const params = await searchParams;
+  const currentPage = Number(params?.page) || 1;
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${currentPage}`,
+  );
+  if (!res.ok) {
+    notFound();
+  }
+  const data: dataProps = await res.json();
 
   return (
-    <div className="">
+    <div>
       {data.results.map((x, i) => (
         <Card
           key={i}
-          id={i}
+          id={x.id}
           name={x.name}
           gender={x.gender}
           status={x.status}
           image={x.image}
         />
       ))}
-      <Link href={`/character/`} className="text-white">
-        link
-      </Link>
-      <FilterBox />
+      <button className="bg-white text-black mr-4">
+        <Link href={`/?page=${1}`}>Click to one</Link>
+      </button>
+      <button className="bg-amber-400 text-black">
+        <Link href={`/?page=${currentPage + 1}`}>Click</Link>
+      </button>
     </div>
   );
 }
